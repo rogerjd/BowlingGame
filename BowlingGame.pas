1cab8eafc59e285fc5c24d7ef30c4b3943059ce6
rjpd@netzero.com <rogerjd>
2019-03-05 13:22:55 -0500

    records, before

--- BowlingGame.pas
+++ BowlingGame.pas
@@ -3,19 +3,58 @@ unit BowlingGame;
 interface
 
 type
-  TFrame = class
+  TFrame = record
   public
     Number: Integer;
     Score: Integer;
     RunningTotal: Integer;
   end;
 
+  TRollResult = (Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine,
+    Strike, Spare);
+
+
+  TRegularFrame = class(TFrame)
+    RollResult1, RollResult2: TRollResult;
+  end;
+
+  TFinalFrame = class(TRegularFrame)
+    RollResult3: TRollResult;
+  end;
+
   TBowlingGame = class
+  private
+    FTotalScore: Integer;
+    procedure SetTotalScore(const Value: Integer);
   public
     procedure Start();
     procedure Roll(NumOfPins: Integer);
+    function ScoreByFrame(): Integer;
+    property TotalScore: Integer read FTotalScore write SetTotalScore;
   end;
 
 implementation
+
+{ TBowlingGame }
+
+procedure TBowlingGame.Roll(NumOfPins: Integer);
+begin
+
+end;
+
+function TBowlingGame.ScoreByFrame: Integer;
+begin
+
+end;
+
+procedure TBowlingGame.SetTotalScore(const Value: Integer);
+begin
+  FTotalScore := Value;
+end;
+
+procedure TBowlingGame.Start;
+begin
+
+end;
 
 end.
